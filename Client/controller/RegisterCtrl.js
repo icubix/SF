9bc4e84cfb1554364d93452536f57e9703f73dfa
shankardotnetshine <shankar.dotnetshine@gmail.com>
2016-11-18 17:17:24 +0530

    added two files

---
+++ Client/controller/RegisterCtrl.js
@@ -0,0 +1,6 @@
+var app = angular.module('sfApp.schoolRegister',[]);
+
+app.controller('register',function($scope,schoolService)
+{
+	alert("helo");
+})
--- Client/route/app.js
+++ Client/route/app.js
@@ -1,9 +1,13 @@
-var app = angular.module('sfApp',['ngRoute','sfApp.schoolfinder']);
+var app = angular.module('sfApp',['ngRoute','sfApp.schoolfinder','sfApp.schoolRegister']);
 app.config(function($routeProvider) {
   $routeProvider
   .when('/', {
     templateUrl : 'views/school.html',
     controller: 'finder'
+  })
+  .when('/Register',{
+  	templateUrl:'views/register.html',
+  	controller:'register'
   })
   .otherwise({redirectTo: '/'});
 });
